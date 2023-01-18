3b91b87d6cd2f39cd86dadbd504bc927e3ec0d40
vidyareddy123456 <vidyareddy0521@gmail.com>
2023-01-18 12:13:58 +0530

    commited

--- Leatest_SF_Project/force-app/main/default/lwc/rendertemplateMethod/rendertemplateMethod.js
+++ Leatest_SF_Project/force-app/main/default/lwc/rendertemplateMethod/rendertemplateMethod.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
+import signinTemplate from './signinTemplate.html'
+import signupTemplate from './signupTemplate.html'
+import renderTemplate from './rendertemplateMethod.html'
+export default class RendertemplateMethod extends LightningElement {
+    selectedBtn=''
+    render(){
+        return this.selectedBtn === 'Signup' ? signupTemplate :
+               this.selectedBtn === 'Signin' ? signinTemplate :
+               renderTemplate
+    }      
 
-export default class RendertemplateMethod extends LightningElement {}
+    handleclick(event){
+      this.selectedBtn = event.target.label
+    }
+
+    submitHandler(event){
+        console.log(`${event.target.label} Successfully!!`)
+    }
+}
